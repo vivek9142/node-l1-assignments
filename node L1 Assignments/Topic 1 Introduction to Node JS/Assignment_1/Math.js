99dd11bb8295baa126bbe36091d38061a6afe6e5
vivek9142 <36185795+vivek9142@users.noreply.github.com>
2021-05-05 19:01:24 +0530

    Add files via upload

---
+++ node L1 Assignments/Topic 1 Introduction to Node JS/Assignment_1/Math.js
@@ -0,0 +1,14 @@
+module.exports = class  {
+     Addition(a,b) {
+        return a + b;
+    } 
+    Subtraction(a,b) {
+        return a - b;
+    } 
+    Multiplication(a,b) {
+        return a * b;
+    } 
+    Division(a,b) {
+        return a / b;
+    } 
+}
